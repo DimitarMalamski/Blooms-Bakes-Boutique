8111d6273f55f6e8fcc18f83f76ba5f49a164fd8
DimitarMalamski <mitemalamata@gmail.com>
2024-03-25 21:17:16 +0200

    Add project files.

---
+++ Blooms & Bakes Boutique/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Blooms___Bakes_Boutique.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
