8b8b9abf15ccdcc72e0aea6b8d4c3601d253fef6
DimitarMalamski <mitemalamata@gmail.com>
2024-04-06 01:57:18 +0300

    IActionResullt Privacy method deleted

--- Blooms & Bakes Boutique/Controllers/HomeController.cs
+++ Blooms & Bakes Boutique/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace Blooms___Bakes_Boutique.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
