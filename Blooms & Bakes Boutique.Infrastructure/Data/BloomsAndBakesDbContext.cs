bd8fadbd2b145252452300b3ba0328e493458f60
DimitarMalamski <mitemalamata@gmail.com>
2024-04-08 00:11:45 +0300

    OnModelCreating method overwritten. OnDelete property made Restrict instead of Cascade

--- Blooms & Bakes Boutique.Infrastructure/Data/BloomsAndBakesDbContext.cs
+++ Blooms & Bakes Boutique.Infrastructure/Data/BloomsAndBakesDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Blooms___Bakes_Boutique.Infrastructure.Data.Models.Flowers;
+using Blooms___Bakes_Boutique.Infrastructure.Data.Models.Pastry;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Blooms___Bakes_Boutique.Infrastructure.Data
@@ -9,5 +11,51 @@ namespace Blooms___Bakes_Boutique.Infrastructure.Data
             : base(options)
         {
         }
+
+        //overwriting OnModelCreating method
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // Pastries
+            builder.Entity<Pastry>()
+                .HasOne(p => p.PastryCategory)
+                .WithMany(pc => pc.Pastries)
+                .HasForeignKey(p => p.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Pastry>()
+                .HasOne(p => p.Patissier)
+                .WithMany(pt => pt.Pastries)
+                .HasForeignKey(p => p.PatissierId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            //Flowers
+            builder.Entity<Flower>()
+                .HasOne(f => f.FlowerCategory)
+                .WithMany(fc => fc.Flowers)
+                .HasForeignKey(f => f.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Flower>()
+                .HasOne(f => f.Florist)
+                .WithMany(fr => fr.Flowers)
+                .HasForeignKey(f => f.FloristId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(builder);
+        }
+
+        // Pastries DbSets
+        public DbSet<Pastry> Pastries { get; set; } = null!;
+
+        public DbSet<PastryCategory> PastriesCategories { get; set; } = null!;
+
+        public DbSet<Patissier> Patissiers { get; set; } = null!;
+
+        // Flowers DbSets
+        public DbSet<Flower> Flowers { get; set; } = null!;
+
+        public DbSet<FlowerCategory> FlowersCategories { get; set; } = null!;
+
+        public DbSet<Florist> Florists { get; set; } = null!;
     }
 }
