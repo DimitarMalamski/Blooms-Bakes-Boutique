5016684ede44caebbae929e6c3baf42be1c859b9
DimitarMalamski <mitemalamata@gmail.com>
2024-04-07 21:36:47 +0300

    Category Entity class finished

--- Blooms & Bakes Boutique.Infrastructure/Data/Models/Category.cs
+++ Blooms & Bakes Boutique.Infrastructure/Data/Models/Category.cs
@@ -5,18 +5,23 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Blooms___Bakes_Boutique.Infrastructure.Constants.DataConstants.Category;
 
 namespace Blooms___Bakes_Boutique.Infrastructure.Data.Models
 {
-    [Comment("Pastry type/category")]
+    [Comment("Pastry Type/Category")]
     public class Category
     {
         [Key]
         [Comment("Category Identifier")]
-        public int Id { get; set; }
+        public int Id { get; init; }
 
         [Required]
-        [MaxLength()]
+        [MaxLength(NameMaxLength)]
+        [Comment("Category Name")]
         public string Name { get; set; } = string.Empty;
+
+        [Comment("Category Pastries")]
+        public IEnumerable<Pastry> Pastries { get; init; } = new List<Pastry>();
     }
 }
