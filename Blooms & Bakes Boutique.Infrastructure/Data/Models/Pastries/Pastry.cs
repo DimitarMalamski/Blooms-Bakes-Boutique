1f2e276b409418738828f85ec488e73b66eac759
DimitarMalamski <mitemalamata@gmail.com>
2024-04-07 23:03:03 +0300

    Pastries folder enity model classes finished

---
+++ Blooms & Bakes Boutique.Infrastructure/Data/Models/Pastries/Pastry.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Blooms___Bakes_Boutique.Infrastructure.Constants.DataConstants.Pastries.Pastry;
+
+namespace Blooms___Bakes_Boutique.Infrastructure.Data.Models.Pastry
+{
+    [Comment("Pastry to taste")]
+    public class Pastry
+    {
+        [Key]
+        [Comment("Pastry Identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        [Comment("Pastry Title")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        [Comment("Pastry Description")]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(RecipeMaxLength)]
+        [Comment("Pastry Recipe")]
+        public string Recipe { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("Pastry Image")]
+        public string ImageUrl { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("Pastry Price")]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Comment("Category Identifier")]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [Comment("Patissier Identifier")]
+        public int PatissierId { get; set; }
+
+        [Comment("User id of the Taster")]
+        public string? TasterId { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        [Comment("Pastry's Category")]
+        public PastryCategory PastryCategory { get; set; } = null!;
+
+        [ForeignKey(nameof(PatissierId))]
+        [Comment("Patissier")]
+        public Patissier Patissier { get; set; } = null!;
+    }
+}
---
+++ Blooms & Bakes Boutique.Infrastructure/Data/Models/Pastries/Patissier.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Blooms___Bakes_Boutique.Infrastructure.Constants.DataConstants.Pastries.Patissier;
+
+namespace Blooms___Bakes_Boutique.Infrastructure.Data.Models.Pastry
+{
+    [Comment("Pastry's Patissier/Chef")]
+    public class Patissier
+    {
+        [Key]
+        [Comment("Patissier Identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(MasterChefMaxLength)]
+        [Comment("Patissier MasterChef Title")]
+        public string MasterChefTitle { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("User Identifier")]
+        public string UserId { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(UserId))]
+        [Comment("User")]
+        public IdentityUser User { get; set; } = null!;
+    }
+}
