fa3a21d106ce516f24550ad8230cd646c862a1f1
DimitarMalamski <mitemalamata@gmail.com>
2024-04-07 21:04:16 +0300

    Category Entity model class started

--- Blooms & Bakes Boutique.Infrastructure/Data/Models/Category.cs
+++ Blooms & Bakes Boutique.Infrastructure/Data/Models/Category.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Blooms___Bakes_Boutique.Infrastructure.Data.Models
 {
+    [Comment("Pastry type/category")]
     public class Category
     {
+        [Key]
+        [Comment("Category Identifier")]
+        public int Id { get; set; }
 
+        [Required]
+        [MaxLength()]
+        public string Name { get; set; } = string.Empty;
     }
 }
