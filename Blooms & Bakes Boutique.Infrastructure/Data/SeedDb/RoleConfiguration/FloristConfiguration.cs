3e7b04052d44182fd0da11c86ac35e64db5dfe7f
DimitarMalamski <mitemalamata@gmail.com>
2024-04-08 23:41:27 +0300

    Patissier and Florist Configuration classes implemented into RoleConfiguration folder

---
+++ Blooms & Bakes Boutique.Infrastructure/Data/SeedDb/RoleConfiguration/FloristConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Blooms___Bakes_Boutique.Infrastructure.Data.Models.Flowers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blooms___Bakes_Boutique.Infrastructure.Data.SeedDb.RoleConfiguration
+{
+    internal class FloristConfiguration : IEntityTypeConfiguration<Florist>
+    {
+        public void Configure(EntityTypeBuilder<Florist> builder)
+        {
+            var data = new SeedData();
+
+            builder.HasData(new Florist[] { data.Florist });
+        }
+    }
+}
---
+++ Blooms & Bakes Boutique.Infrastructure/Data/SeedDb/RoleConfiguration/PatissierConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Blooms___Bakes_Boutique.Infrastructure.Data.Models.Pastry;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blooms___Bakes_Boutique.Infrastructure.Data.SeedDb.RoleConfiguration
+{
+    internal class PatissierConfiguration : IEntityTypeConfiguration<Patissier>
+    {
+        public void Configure(EntityTypeBuilder<Patissier> builder)
+        {
+            var data = new SeedData();
+
+            builder.HasData(new Patissier[] { data.Patissier });
+        }
+    }
+}
