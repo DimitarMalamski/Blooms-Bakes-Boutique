4205755d26033b7c5ff62c2b9e932dba731faf13
DimitarMalamski <mitemalamata@gmail.com>
2024-04-08 23:41:44 +0300

    User configiration implemented

---
+++ Blooms & Bakes Boutique.Infrastructure/Data/SeedDb/UserConfiguration/UserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blooms___Bakes_Boutique.Infrastructure.Data.SeedDb.UserConfiguration
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<IdentityUser>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUser> builder)
+        {
+            var data = new SeedData();
+
+            builder.HasData(new IdentityUser[] { data.PatissierUser, data.FloristUser, data.GuestUser});
+        }
+    }
+}
