57825ed590e3773850e80cd0f26a2fb26a6bd266
DimitarMalamski <mitemalamata@gmail.com>
2024-04-08 23:40:58 +0300

    Pastry and Flower Configuration classes implemented into ProductConfiguration folder

---
+++ Blooms & Bakes Boutique.Infrastructure/Data/SeedDb/ProductConfiguration/FlowerConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Blooms___Bakes_Boutique.Infrastructure.Data.Models.Flowers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blooms___Bakes_Boutique.Infrastructure.Data.SeedDb.ProductConfiguration
+{
+    internal class FlowerConfiguration : IEntityTypeConfiguration<Flower>
+    {
+        public void Configure(EntityTypeBuilder<Flower> builder)
+        {
+            builder
+                .HasOne(f => f.FlowerCategory)
+                .WithMany(fc => fc.Flowers)
+                .HasForeignKey(f => f.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(f => f.Florist)
+                .WithMany(fr => fr.Flowers)
+                .HasForeignKey(f => f.FloristId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            var data = new SeedData();
+
+            builder.HasData(new Flower[] { data.FirstFlower, data.SecondFlower, data.ThirdFlower });
+        }
+    }
+}
---
+++ Blooms & Bakes Boutique.Infrastructure/Data/SeedDb/ProductConfiguration/PastryConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Blooms___Bakes_Boutique.Infrastructure.Data.Models.Pastry;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blooms___Bakes_Boutique.Infrastructure.Data.SeedDb.ProductConfiguration
+{
+    internal class PastryConfiguration : IEntityTypeConfiguration<Pastry>
+    {
+        public void Configure(EntityTypeBuilder<Pastry> builder)
+        {
+            builder
+                .HasOne(p => p.PastryCategory)
+                .WithMany(pc => pc.Pastries)
+                .HasForeignKey(p => p.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(p => p.Patissier)
+                .WithMany(pt => pt.Pastries)
+                .HasForeignKey(p => p.PatissierId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            var data = new SeedData();
+
+            builder.HasData(new Pastry[] { data.FirstPastry, data.SecondPastry, data.ThirdPastry });
+        }
+    }
+}
