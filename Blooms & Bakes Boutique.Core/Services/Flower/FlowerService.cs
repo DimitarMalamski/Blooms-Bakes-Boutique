bc4618fa5306d1fb45f36ee7038d292840122d30
DimitarMalamski <mitemalamata@gmail.com>
2024-04-10 17:18:32 +0300

    FlowerService inherits IFlowerService interface

--- Blooms & Bakes Boutique.Core/Services/Flower/FlowerService.cs
+++ Blooms & Bakes Boutique.Core/Services/Flower/FlowerService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blooms___Bakes_Boutique.Core.Contracts.Flower;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Blooms___Bakes_Boutique.Core.Services.Flower
 {
-    public class FlowerService
+    public class FlowerService : IFlowerService
     {
+
     }
 }
