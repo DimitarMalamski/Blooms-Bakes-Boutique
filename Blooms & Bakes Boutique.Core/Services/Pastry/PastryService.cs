892fce34c3790f6bdfe614380694a408a8f1731e
DimitarMalamski <mitemalamata@gmail.com>
2024-04-10 17:15:56 +0300

    Pastry and Flower service classes added to Services folder

---
+++ Blooms & Bakes Boutique.Core/Services/Pastry/PastryService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blooms___Bakes_Boutique.Core.Services.Pastry
+{
+    public class PastryService
+    {
+    }
+}
