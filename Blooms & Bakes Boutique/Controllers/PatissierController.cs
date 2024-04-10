04b419347e8ead2f1e96166ea1bd9d6e01d7632b
DimitarMalamski <mitemalamata@gmail.com>
2024-04-10 20:57:53 +0300

    PatissierController added

--- Blooms & Bakes Boutique/Controllers/PatissierController.cs
+++ Blooms & Bakes Boutique/Controllers/PatissierController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blooms___Bakes_Boutique.Core.Models.Patissier;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blooms___Bakes_Boutique.Controllers
 {
+    [Authorize]
     public class PatissierController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> BecomePatissier()
         {
-            return View();
+            var model = new BecomePatissierFormModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> BecomePatissier(BecomePatissierFormModel model)
+        {
+            return RedirectToAction(nameof(PastryController.AllPastry), "Pastry");
         }
     }
 }
