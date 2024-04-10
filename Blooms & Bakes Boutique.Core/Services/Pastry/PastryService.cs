1d48b847dd4cefbd89ce1df102ff693d735bd723
DimitarMalamski <mitemalamata@gmail.com>
2024-04-10 17:18:47 +0300

    PastryService inherits IPastryService interface

--- Blooms & Bakes Boutique.Core/Services/Pastry/PastryService.cs
+++ Blooms & Bakes Boutique.Core/Services/Pastry/PastryService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blooms___Bakes_Boutique.Core.Contracts.Pastry;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Blooms___Bakes_Boutique.Core.Services.Pastry
 {
-    public class PastryService
+    public class PastryService : IPastryService
     {
+
     }
 }
