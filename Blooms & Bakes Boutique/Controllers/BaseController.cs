070d3af48fc9e5b2181401bc13490dd93c8fd09f
DimitarMalamski <mitemalamata@gmail.com>
2024-04-10 21:22:09 +0300

    BaseController with [Authorize] attribute added

---
+++ Blooms & Bakes Boutique/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blooms___Bakes_Boutique.Controllers
+{
+	[Authorize]
+	public class BaseController : Controller
+	{
+
+	}
+}
