01d84cde739227f88d2293ecf1b1c4cc1da0e57b
DimitarMalamski <mitemalamata@gmail.com>
2024-04-10 20:57:34 +0300

    FlowerController methods implemented

--- Blooms & Bakes Boutique/Controllers/FlowerController.cs
+++ Blooms & Bakes Boutique/Controllers/FlowerController.cs
@@ -1,12 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blooms___Bakes_Boutique.Core.Models.Flower;
+using Blooms___Bakes_Boutique.Core.Models.Pastry;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blooms___Bakes_Boutique.Controllers
 {
+    [Authorize]
     public class FlowerController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+        [AllowAnonymous]
+		public async Task<IActionResult> AllFlower()
+		{
+			return View(new AllFlowersQueryModel());
+		}
+		public async Task<IActionResult> MineFlower()
+		{
+			return View(new AllFlowersQueryModel());
+		}
+
+		public async Task<IActionResult> FlowerDetails(int id)
+		{
+			return View(new FlowerDetailsViewModel());
+		}
+
+		public async Task<IActionResult> AddFlower()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> AddFlower(FlowerFormModel model)
+		{
+			return RedirectToAction(nameof(FlowerDetails), new { id = "1" });
+		}
+		public async Task<IActionResult> EditFlower(int id)
+		{
+			return View(new FlowerFormModel());
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> EditFlower(int id, FlowerFormModel model)
+		{
+			return RedirectToAction(nameof(FlowerDetails), new { id = "1" });
+		}
+		public async Task<IActionResult> DeleteFlower(int id)
+		{
+			return View(new FlowerFormModel());
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> DeleteFlower(FlowerDetailsViewModel model)
+		{
+			return RedirectToAction(nameof(AllFlower));
+		}
+	}
 }
