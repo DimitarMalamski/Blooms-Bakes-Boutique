4da3829522b74a3b8e01fd71d268aa605010cd2b
DimitarMalamski <mitemalamata@gmail.com>
2024-04-10 20:52:31 +0300

    FlowerFormModel added to Models/Flower

---
+++ Blooms & Bakes Boutique.Core/Models/Flower/FlowerFormModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blooms___Bakes_Boutique.Core.Models.Flower
+{
+	public class FlowerFormModel
+	{
+
+	}
+}
