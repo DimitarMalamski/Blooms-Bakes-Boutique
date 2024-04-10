3be3f524979d3694138cf01ad363a7bd44087401
DimitarMalamski <mitemalamata@gmail.com>
2024-04-10 17:50:07 +0300

    All and AllReadOnlt methods implemented

--- Blooms & Bakes Boutique.Infrastructure/Data/Common/IRepository.cs
+++ Blooms & Bakes Boutique.Infrastructure/Data/Common/IRepository.cs
@@ -8,6 +8,8 @@ namespace Blooms___Bakes_Boutique.Infrastructure.Data.Common
 {
     public interface IRepository
     {
+        IQueryable<T> All<T>();
 
+        IQueryable<T> AllReadOnly<T>();
     }
 }
