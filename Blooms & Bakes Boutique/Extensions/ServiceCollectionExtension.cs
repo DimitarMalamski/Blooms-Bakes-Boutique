4313479fc67d674d6c45a592815d97d51f405c14
DimitarMalamski <mitemalamata@gmail.com>
2024-04-10 20:56:55 +0300

    Repositories added to ServiceCollectionExtension class

--- Blooms & Bakes Boutique/Extensions/ServiceCollectionExtension.cs
+++ Blooms & Bakes Boutique/Extensions/ServiceCollectionExtension.cs
@@ -1,7 +1,11 @@
-﻿using Blooms___Bakes_Boutique.Core.Contracts.Flower;
+﻿using Blooms___Bakes_Boutique.Core.Contracts.Florist;
+using Blooms___Bakes_Boutique.Core.Contracts.Flower;
 using Blooms___Bakes_Boutique.Core.Contracts.Pastry;
+using Blooms___Bakes_Boutique.Core.Contracts.Patissier;
+using Blooms___Bakes_Boutique.Core.Services.Florist;
 using Blooms___Bakes_Boutique.Core.Services.Flower;
 using Blooms___Bakes_Boutique.Core.Services.Pastry;
+using Blooms___Bakes_Boutique.Core.Services.Patissier;
 using Blooms___Bakes_Boutique.Infrastructure.Data;
 using Blooms___Bakes_Boutique.Infrastructure.Data.Common;
 using Microsoft.AspNetCore.Identity;
@@ -16,6 +20,8 @@ namespace Microsoft.Extensions.DependencyInjection
         {
             services.AddScoped<IPastryService, PastryService>();
             services.AddScoped<IFlowerService, FlowerService>();
+            services.AddScoped<IPatissierService, PatissierService>();
+            services.AddScoped<IFloristService, FloristService>();
 
             return services;
         }
