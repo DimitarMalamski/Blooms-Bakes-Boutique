18aec21f232f80e5d414f4eb5f89c1c33efb73c8
DimitarMalamski <mitemalamata@gmail.com>
2024-04-10 20:57:45 +0300

    FloristController added

--- Blooms & Bakes Boutique/Controllers/FloristController.cs
+++ Blooms & Bakes Boutique/Controllers/FloristController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blooms___Bakes_Boutique.Core.Models.Florist;
+using Blooms___Bakes_Boutique.Core.Models.Patissier;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blooms___Bakes_Boutique.Controllers
 {
+    [Authorize]
     public class FloristController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+		[HttpGet]
+		public async Task<IActionResult> BecomeFlorist()
+		{
+			var model = new BecomeFloristFormModel();
+
+			return View(model);
+		}
+
+		[HttpPost]
+
+		public async Task<IActionResult> BecomeFlorist(BecomeFloristFormModel model)
+		{
+			return RedirectToAction(nameof(FlowerController.AllFlower), "Flower");
+		}
+	}
 }
