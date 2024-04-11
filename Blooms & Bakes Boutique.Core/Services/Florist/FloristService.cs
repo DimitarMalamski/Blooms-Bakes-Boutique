e8307922ae5f6757e93b43f086c68eac3b5be06b
DimitarMalamski <mitemalamata@gmail.com>
2024-04-11 15:22:47 +0300

    Methods implemented in FloristService class

--- Blooms & Bakes Boutique.Core/Services/Florist/FloristService.cs
+++ Blooms & Bakes Boutique.Core/Services/Florist/FloristService.cs
@@ -12,25 +12,33 @@ namespace Blooms___Bakes_Boutique.Core.Services.Florist
 			repository = _repository;
 		}
 
-		public Task CreateAsync(string userId, string flowerMasterTitle)
+		public async Task CreateAsync(string userId, string flowerMasterTitle)
 		{
-			throw new NotImplementedException();
+			await repository.AddAsync(new Blooms___Bakes_Boutique.Infrastructure.Data.Models.Flowers.Florist()
+			{
+				UserId = userId,
+				FlowerMasterTitle = flowerMasterTitle
+			});
+
+			await repository.SaveChangesAsync();
 		}
 
 		public async Task<bool> ExistByIdAsync(string userId)
 		{
 			return await repository.AllReadOnly<Blooms___Bakes_Boutique.Infrastructure.Data.Models.Flowers.Florist>()
-				.AnyAsync(a => a.UserId == userId);
+				.AnyAsync(fl => fl.UserId == userId);
 		}
 
-		public Task<bool> UserHasGatheredFlowersAsync(string userId)
+		public async Task<bool> UserHasGatheredFlowersAsync(string userId)
 		{
-			throw new NotImplementedException();
+			return await repository.AllReadOnly<Blooms___Bakes_Boutique.Infrastructure.Data.Models.Flowers.Flower>()
+				.AnyAsync(f => f.GathererId == userId);
 		}
 
-		public Task<bool> UserWithFlowerMasterTitleExistsAsync(string flowerMasterTitle)
+		public async Task<bool> UserWithFlowerMasterTitleExistsAsync(string flowerMasterTitle)
 		{
-			throw new NotImplementedException();
+			return await repository.AllReadOnly<Blooms___Bakes_Boutique.Infrastructure.Data.Models.Flowers.Florist>()
+				.AnyAsync(fl => fl.FlowerMasterTitle == flowerMasterTitle);
 		}
 	}
 }
