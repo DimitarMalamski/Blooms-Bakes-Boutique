ac806c3c0a67717d16f8e5c5bfff57f2fd7cae29
DimitarMalamski <mitemalamata@gmail.com>
2024-04-11 17:52:29 +0300

    BecomeFlorirstFormModel class finished

--- Blooms & Bakes Boutique.Core/Models/Florist/BecomeFloristFormModel.cs
+++ Blooms & Bakes Boutique.Core/Models/Florist/BecomeFloristFormModel.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Blooms___Bakes_Boutique.Core.Constants.MessageConstants;
+using static Blooms___Bakes_Boutique.Infrastructure.Constants.DataConstants.Flowers.Florsit;
 
 namespace Blooms___Bakes_Boutique.Core.Models.Florist
 {
 	public class BecomeFloristFormModel
 	{
-
+		[Required(ErrorMessage = RequiredMessage)]
+		[StringLength(FlowerMasterMaxLength,
+			MinimumLength = FlowerMasterMinLength,
+			ErrorMessage = LengthMessage)]
+		[Display(Name = "FlowerMaster Title")]
+		[RegularExpression(@"(?=.*?[A-Z]).*", ErrorMessage = RegexErrorMessage)]
+		public string FlowerMasterTitle { get; set; } = null!;
 	}
 }
