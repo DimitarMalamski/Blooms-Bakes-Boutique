0800ddde94bf4272dbc3781d684f906605dd6137
DimitarMalamski <mitemalamata@gmail.com>
2024-04-11 14:49:43 +0300

    ExistById added to Patissier and Florist Service class

--- Blooms & Bakes Boutique.Core/Services/Florist/FloristService.cs
+++ Blooms & Bakes Boutique.Core/Services/Florist/FloristService.cs
@@ -1,14 +1,20 @@
 ﻿using Blooms___Bakes_Boutique.Core.Contracts.Florist;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Blooms___Bakes_Boutique.Infrastructure.Data.Common;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blooms___Bakes_Boutique.Core.Services.Florist
 {
-    public class FloristService : IFloristService
-    {
-
-    }
+	public class FloristService : IFloristService
+	{
+		private readonly IRepository repository;
+		public FloristService(IRepository _repository)
+		{
+			repository = _repository;
+		}
+		public async Task<bool> ExistById(string userId)
+		{
+			return await repository.AllReadOnly<Blooms___Bakes_Boutique.Infrastructure.Data.Models.Flowers.Florist>()
+				.AnyAsync(a => a.UserId == userId);
+		}
+	}
 }
--- Blooms & Bakes Boutique.Core/Services/Patissier/PatissierService.cs
+++ Blooms & Bakes Boutique.Core/Services/Patissier/PatissierService.cs
@@ -1,4 +1,6 @@
 ﻿using Blooms___Bakes_Boutique.Core.Contracts.Patissier;
+using Blooms___Bakes_Boutique.Infrastructure.Data.Common;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +9,18 @@ using System.Threading.Tasks;
 
 namespace Blooms___Bakes_Boutique.Core.Services.Patissier
 {
-    public class PatissierService : IPatissierService
-    {
+	public class PatissierService : IPatissierService
+	{
+		private readonly IRepository repository;
 
-    }
+		public PatissierService(IRepository _repository)
+		{
+			repository = _repository;
+		}
+		public async Task<bool> ExistById(string userId)
+		{
+			return await repository.AllReadOnly<Blooms___Bakes_Boutique.Infrastructure.Data.Models.Pastry.Patissier>()
+				.AnyAsync(a => a.UserId == userId);
+		}
+	}
 }
