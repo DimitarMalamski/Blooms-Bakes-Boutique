2944f5faabacc9d404ba969c80e1c9900cd3802d
DimitarMalamski <mitemalamata@gmail.com>
2024-04-11 16:35:02 +0300

    Class ovverride method implemented

--- Blooms & Bakes Boutique/Attributes/NotAFloristAttribute.cs
+++ Blooms & Bakes Boutique/Attributes/NotAFloristAttribute.cs
@@ -1,6 +1,30 @@
-﻿namespace Blooms___Bakes_Boutique.Attributes
+﻿using Blooms___Bakes_Boutique.Core.Contracts.Florist;
+using Blooms___Bakes_Boutique.Core.Contracts.Patissier;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace Blooms___Bakes_Boutique.Attributes
 {
-	public class NotAFloristAttribute
+	public class NotAFloristAttribute : ActionFilterAttribute
 	{
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			base.OnActionExecuting(context);
+
+			IFloristService? floristService = context.HttpContext.RequestServices.GetService<IFloristService>();
+
+			if (floristService == null)
+			{
+				context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+			}
+
+			if (floristService != null
+				&& floristService.ExistByIdAsync(context.HttpContext.User.Id()).Result)
+			{
+				context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+			}
+
+		}
 	}
 }
--- Blooms & Bakes Boutique/Attributes/NotAPatissierAttribute.cs
+++ Blooms & Bakes Boutique/Attributes/NotAPatissierAttribute.cs
@@ -1,6 +1,29 @@
-﻿namespace Blooms___Bakes_Boutique.Attributes
+﻿using Blooms___Bakes_Boutique.Core.Contracts.Patissier;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace Blooms___Bakes_Boutique.Attributes
 {
-	public class NotAPatissierAttribute
+	public class NotAPatissierAttribute : ActionFilterAttribute
 	{
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			base.OnActionExecuting(context);
+
+			IPatissierService? patissierService = context.HttpContext.RequestServices.GetService<IPatissierService>();
+
+			if (patissierService == null)
+			{
+				context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+			}
+
+			if (patissierService != null
+				&& patissierService.ExistByIdAsync(context.HttpContext.User.Id()).Result)
+			{
+				context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+			}
+
+		}
 	}
 }
