62c2b77160df4d92aa2c67ca434d93cf59dae6a6
DimitarMalamski <mitemalamata@gmail.com>
2024-04-11 16:24:29 +0300

    MessageContants class added and 3 const error messages strings added to validate the attribute imputs

---
+++ Blooms & Bakes Boutique.Core/Constants/MessageConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Blooms___Bakes_Boutique.Core.Constants
+{
+	public static class MessageConstants
+	{
+		public const string RequiredMessage = "The {0} field is required.";
+
+		public const string LengthMessage = "The field {0} must be between {2} and {1} characters long.";
+
+		public const string RegexErrorMessage = "The field {0} must contain at least one capital letter.";
+	}
+}
