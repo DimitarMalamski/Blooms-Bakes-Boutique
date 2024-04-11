87c920563e535490b3bb46d50426c74705dce52c
DimitarMalamski <mitemalamata@gmail.com>
2024-04-11 14:38:02 +0300

    ClaimPrincipalExtemsions class added with method Id

---
+++ Blooms & Bakes Boutique/Extensions/ClaimPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace Blooms___Bakes_Boutique.Extensions
+{
+	public static class ClaimPrincipalExtensions
+	{
+		public static string Id(this ClaimsPrincipal user)
+		{
+			return user.FindFirstValue(ClaimTypes.NameIdentifier);
+		}
+	}
+}
