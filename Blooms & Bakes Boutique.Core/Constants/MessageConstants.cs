1a4a09252c3b5d00b658adea4251c124cfe1b9fa
DimitarMalamski <mitemalamata@gmail.com>
2024-04-11 17:52:07 +0300

    new string const messages added to validate input

--- Blooms & Bakes Boutique.Core/Constants/MessageConstants.cs
+++ Blooms & Bakes Boutique.Core/Constants/MessageConstants.cs
@@ -2,10 +2,18 @@
 {
 	public static class MessageConstants
 	{
-		public const string RequiredMessage = "The {0} field is required.";
+		public const string RequiredMessage = "The {0} field is required!";
 
-		public const string LengthMessage = "The field {0} must be between {2} and {1} characters long.";
+		public const string LengthMessage = "The field {0} must be between {2} and {1} characters long!";
 
-		public const string RegexErrorMessage = "The field {0} must contain at least one capital letter.";
+		public const string RegexErrorMessage = "The field {0} must contain at least one capital letter!";
+
+		public const string FlowerMasterTitleExists = "FlowerMaster title already exists! Enter another one.";
+
+		public const string HasGathered = "You should have no gathered flowers to become a florist!";
+
+		public const string MasterChefTitleExists = "MasterChef title already exists! Enter another one.";
+
+		public const string HasTasted = "You should have no tasted pastries to become a patissier!";
 	}
 }
