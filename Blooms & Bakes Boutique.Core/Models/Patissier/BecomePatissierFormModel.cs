6222093c18592b5b823c720bbd1108b10535ecb7
DimitarMalamski <mitemalamata@gmail.com>
2024-04-11 16:24:51 +0300

    BecomePatissierFormModel finished

--- Blooms & Bakes Boutique.Core/Models/Patissier/BecomePatissierFormModel.cs
+++ Blooms & Bakes Boutique.Core/Models/Patissier/BecomePatissierFormModel.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using static Blooms___Bakes_Boutique.Core.Constants.MessageConstants;
+using static Blooms___Bakes_Boutique.Infrastructure.Constants.DataConstants.Pastries.Patissier;
 
 namespace Blooms___Bakes_Boutique.Core.Models.Patissier
 {
     public class BecomePatissierFormModel
     {
-
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(MasterChefMaxLength,
+            MinimumLength = MasterChefMinLength,
+            ErrorMessage = LengthMessage)]
+        [Display(Name = "MasterChef Title")]
+        [RegularExpression(@"/[A-Z]/", ErrorMessage = RegexErrorMessage)]
+        public string MasterChefTitle { get; set; } = null!;
     }
 }
