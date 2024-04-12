86fcbc5ed838ceacd513a5298a6e7e13967b4311
DimitarMalamski <mitemalamata@gmail.com>
2024-04-12 14:49:32 +0300

    MustBeAttributes added

---
+++ Blooms & Bakes Boutique/Attributes/MustBeFloristAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Blooms___Bakes_Boutique.Core.Contracts.Florist;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Blooms___Bakes_Boutique.Controllers;
+
+namespace Blooms___Bakes_Boutique.Attributes
+{
+	public class MustBeFloristAttribute : ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			base.OnActionExecuting(context);
+
+			IFloristService? floristService = context.HttpContext.RequestServices.GetService<IFloristService>();
+
+			if (floristService == null)
+			{
+				context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+			}
+
+			if (floristService != null
+				&& floristService.ExistByIdAsync(context.HttpContext.User.Id()).Result == false)
+			{
+				context.Result = new RedirectToActionResult(nameof(FloristController.BecomeFlorist), "Florist", null);
+			}
+
+		}
+	}
+}
---
+++ Blooms & Bakes Boutique/Attributes/MustBePatissierAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Blooms___Bakes_Boutique.Core.Contracts.Patissier;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Blooms___Bakes_Boutique.Controllers;
+
+namespace Blooms___Bakes_Boutique.Attributes
+{
+	public class MustBePatissierAttribute : ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			base.OnActionExecuting(context);
+
+			IPatissierService? patissierService = context.HttpContext.RequestServices.GetService<IPatissierService>();
+
+			if (patissierService == null)
+			{
+				context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+			}
+
+			if (patissierService != null
+				&& patissierService.ExistByIdAsync(context.HttpContext.User.Id()).Result == false)
+			{
+				context.Result = new RedirectToActionResult(nameof(PatissierController.BecomePatissier), "Patissier", null);
+			}
+
+		}
+	}
+}
