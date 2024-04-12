1f42fcf8488cf9375d5ac57a2702877e69722462
DimitarMalamski <mitemalamata@gmail.com>
2024-04-12 14:52:31 +0300

    Update

---
+++ Blooms & Bakes Boutique.Core/Models/Flower/FlowerCategoryServiceModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blooms___Bakes_Boutique.Core.Models.Flower
+{
+	public class FlowerCategoryServiceModel
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; } = string.Empty;
+	}
+}
