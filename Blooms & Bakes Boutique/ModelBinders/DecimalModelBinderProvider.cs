8671190d88cdbf51ee0f2ce1e486f59fdb3e5bde
DimitarMalamski <mitemalamata@gmail.com>
2024-04-12 12:03:23 +0300

    Decimal Model binder and Decimal Model Binder Provider classed added to ModelBinder folder

---
+++ Blooms & Bakes Boutique/ModelBinders/DecimalModelBinderProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Blooms___Bakes_Boutique.ModelBinders
+{
+	public class DecimalModelBinderProvider : IModelBinderProvider
+	{
+		public IModelBinder? GetBinder(ModelBinderProviderContext context)
+		{
+			if (context == null)
+			{
+				throw new ArgumentNullException(nameof(context));
+			}
+
+			if (context.Metadata.ModelType == typeof(decimal)
+				|| context.Metadata.ModelType == typeof(decimal?))
+			{
+				return new DecimalModelBinder();
+			}
+
+			return null;
+		}
+	}
+}
