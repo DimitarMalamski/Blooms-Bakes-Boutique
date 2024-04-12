7e0ec09002d9c5f2bb3f453559eb71327ca44fff
DimitarMalamski <mitemalamata@gmail.com>
2024-04-12 14:50:45 +0300

    Add supporting methods implemented

--- Blooms & Bakes Boutique.Core/Contracts/Pastry/IPastryService.cs
+++ Blooms & Bakes Boutique.Core/Contracts/Pastry/IPastryService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blooms___Bakes_Boutique.Core.Models.Pastry;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,10 @@ namespace Blooms___Bakes_Boutique.Core.Contracts.Pastry
 {
     public interface IPastryService
     {
+        Task<IEnumerable<PastryCategoryServiceModel>> AllPastryCategoriesAsync();
 
-    }
+        Task<bool> PastryCategoryExistsAsync(int categoryId);
+
+        Task<int> CreateAsync(PastryFormModel model, int patissierId);
+	}
 }
