fbd5c8629d406d057cc9846cafaf0ef51db1a6f8
DimitarMalamski <mitemalamata@gmail.com>
2024-04-12 14:52:25 +0300

    Methods implemented into the Pastry Service class

--- Blooms & Bakes Boutique.Core/Services/Pastry/PastryService.cs
+++ Blooms & Bakes Boutique.Core/Services/Pastry/PastryService.cs
@@ -1,14 +1,12 @@
 ﻿using Blooms___Bakes_Boutique.Core.Contracts.Pastry;
+using Blooms___Bakes_Boutique.Core.Models.Pastry;
 using Blooms___Bakes_Boutique.Infrastructure.Data.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Blooms___Bakes_Boutique.Infrastructure.Data.Models.Pastries;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blooms___Bakes_Boutique.Core.Services.Pastry
 {
-    public class PastryService : IPastryService
+	public class PastryService : IPastryService
     {
         private readonly IRepository repository;
 
@@ -16,5 +14,41 @@ namespace Blooms___Bakes_Boutique.Core.Services.Pastry
         {
             repository = _repository;
         }
-    }
+
+		public async Task<IEnumerable<PastryCategoryServiceModel>> AllPastryCategoriesAsync()
+		{
+            return await repository.AllReadOnly<PastryCategory>()
+                .Select(pc => new PastryCategoryServiceModel()
+                {
+                    Id = pc.Id,
+                    Name = pc.Name
+                })
+                .ToListAsync();
+		}
+
+		public async Task<int> CreateAsync(PastryFormModel model, int patissierId)
+		{
+            Infrastructure.Data.Models.Pastries.Pastry pastry = new Infrastructure.Data.Models.Pastries.Pastry()
+            {
+                Description = model.Description,
+                PatissierId = patissierId,
+                CategoryId = model.CategoryId,
+                Recipe = model.Recipe,
+                ImageUrl = model.ImageUrl,
+                Price = model.Price,
+                Title = model.Title
+            };
+
+            await repository.AddAsync(pastry);
+            await repository.SaveChangesAsync();
+
+            return pastry.Id;
+		}
+
+		public async Task<bool> PastryCategoryExistsAsync(int categoryId)
+		{
+            return await repository.AllReadOnly<PastryCategory>()
+                .AnyAsync(pc => pc.Id == categoryId);
+		}
+	}
 }
