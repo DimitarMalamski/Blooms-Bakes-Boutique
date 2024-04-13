ade9c37e3e8865535c90ea34b6f7aa00888c8937
DimitarMalamski <mitemalamata@gmail.com>
2024-04-13 17:19:07 +0300

    Update

--- Blooms & Bakes Boutique.Core/Contracts/Flower/IFlowerService.cs
+++ Blooms & Bakes Boutique.Core/Contracts/Flower/IFlowerService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blooms___Bakes_Boutique.Core.Models.Pastry;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,10 @@ namespace Blooms___Bakes_Boutique.Core.Contracts.Flower
 {
     public interface IFlowerService
     {
+		Task<IEnumerable<PastryCategoryServiceModel>> AllFlowerCategoriesAsync();
 
-    }
+		Task<bool> FlowerCategoryExistsAsync(int categoryId);
+
+		Task<int> CreateAsync(PastryFormModel model, int floristId);
+	}
 }
--- Blooms & Bakes Boutique.Core/Services/Flower/FlowerService.cs
+++ Blooms & Bakes Boutique.Core/Services/Flower/FlowerService.cs
@@ -1,4 +1,5 @@
 ﻿using Blooms___Bakes_Boutique.Core.Contracts.Flower;
+using Blooms___Bakes_Boutique.Core.Models.Pastry;
 using Blooms___Bakes_Boutique.Infrastructure.Data.Common;
 using System;
 using System.Collections.Generic;
@@ -16,5 +17,20 @@ namespace Blooms___Bakes_Boutique.Core.Services.Flower
         {
             repository = _repository;
         }
-    }
+
+		public Task<IEnumerable<PastryCategoryServiceModel>> AllFlowerCategoriesAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<int> CreateAsync(PastryFormModel model, int floristId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> FlowerCategoryExistsAsync(int categoryId)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
