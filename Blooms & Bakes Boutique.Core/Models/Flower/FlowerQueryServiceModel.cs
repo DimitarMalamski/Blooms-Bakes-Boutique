a7d64e2ff5719d3715f1386c4922e56eceae0a39
DimitarMalamski <mitemalamata@gmail.com>
2024-04-13 23:37:11 +0300

    FlowerQueryServiceModel

---
+++ Blooms & Bakes Boutique.Core/Models/Flower/FlowerQueryServiceModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Blooms___Bakes_Boutique.Core.Models.Flower
+{
+	public class FlowerQueryServiceModel
+	{
+		public int TotalFlowersCount { get; set; }
+
+		public IEnumerable<FlowerServiceModel> Flowers { get; set; } =
+			new List<FlowerServiceModel>();
+	}
+}
