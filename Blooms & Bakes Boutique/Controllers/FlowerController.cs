1d90ef23b10f1a651fe8d4e4eadde7db938c3d74
DimitarMalamski <mitemalamata@gmail.com>
2024-04-13 17:59:48 +0300

    Add method finihsed in FlowerController

--- Blooms & Bakes Boutique/Controllers/FlowerController.cs
+++ Blooms & Bakes Boutique/Controllers/FlowerController.cs
@@ -1,13 +1,33 @@
-﻿using Blooms___Bakes_Boutique.Core.Models.Flower;
+﻿using Blooms___Bakes_Boutique.Attributes;
+using Blooms___Bakes_Boutique.Core.Contracts.Florist;
+using Blooms___Bakes_Boutique.Core.Contracts.Flower;
+using Blooms___Bakes_Boutique.Core.Contracts.Pastry;
+using Blooms___Bakes_Boutique.Core.Contracts.Patissier;
+using Blooms___Bakes_Boutique.Core.Models.Flower;
 using Blooms___Bakes_Boutique.Core.Models.Pastry;
+using Blooms___Bakes_Boutique.Core.Services.Pastry;
+using Blooms___Bakes_Boutique.Core.Services.Patissier;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using static Blooms___Bakes_Boutique.Core.Constants.MessageConstants;
 
 namespace Blooms___Bakes_Boutique.Controllers
 {
     public class FlowerController : BaseController
     {
-        [AllowAnonymous]
+		private readonly IFlowerService flowerService;
+		private readonly IFloristService floristService;
+
+		public FlowerController(
+			IFlowerService _flowerService,
+			IFloristService _floristService)
+		{
+			flowerService = _flowerService;
+			floristService = _floristService;
+		}
+
+		[AllowAnonymous]
 		[HttpGet]
 		public async Task<IActionResult> AllFlower()
 		{
@@ -27,15 +47,38 @@ namespace Blooms___Bakes_Boutique.Controllers
 		}
 
 		[HttpGet]
+		[MustBeFlorist]
 		public async Task<IActionResult> AddFlower()
 		{
-			return View();
+			var model = new FlowerFormModel()
+			{
+				FlowerCategories = await flowerService.AllFlowerCategoriesAsync()
+			};
+
+			return View(model);
 		}
 
 		[HttpPost]
+		[MustBeFlorist]
 		public async Task<IActionResult> AddFlower(FlowerFormModel model)
 		{
-			return RedirectToAction(nameof(FlowerDetails), new { id = "1" });
+			if (await flowerService.FlowerCategoryExistsAsync(model.CategoryId) == false)
+			{
+				ModelState.AddModelError(nameof(model.CategoryId), FlowerCategoryDoesNotExist);
+			}
+
+			if (ModelState.IsValid == false)
+			{
+				model.FlowerCategories = await flowerService.AllFlowerCategoriesAsync();
+
+				return View(model);
+			}
+
+			int? floristId = await floristService.GetFloristIdAsync(User.Id());
+
+			int newFlowerId = await flowerService.CreateAsync(model, floristId ?? 0);
+
+			return RedirectToAction(nameof(FlowerDetails), new { id = newFlowerId });
 		}
 
 		[HttpGet]
