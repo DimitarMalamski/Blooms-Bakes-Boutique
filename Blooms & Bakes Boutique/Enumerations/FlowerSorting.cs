9c7578c3c99a26a22eea2c709e5a425f06068da8
DimitarMalamski <mitemalamata@gmail.com>
2024-04-13 23:35:07 +0300

    Enumration folder switched places

--- Blooms & Bakes Boutique/Enumerations/FlowerSorting.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Blooms___Bakes_Boutique.Enumerations
-{
-	public enum FlowerSorting
-	{
-		Newest = 0,
-		PricePerBouquet = 1,
-		NotGatheredFirst = 2
-	}
-}
--- Blooms & Bakes Boutique/Enumerations/PastrySorting.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Blooms___Bakes_Boutique.Enumerations
-{
-	public enum PastrySorting
-	{
-		Newest = 0,
-		PriceOfPastry = 1,
-		NotTastedFirst = 2
-	}
-}
