3876a7fa1eeeb18cf5d52c175a090b6c29d8067f
DimitarMalamski <mitemalamata@gmail.com>
2024-04-13 23:36:50 +0300

    new methods added into Pastry service interface

--- Blooms & Bakes Boutique.Core/Contracts/Pastry/IPastryService.cs
+++ Blooms & Bakes Boutique.Core/Contracts/Pastry/IPastryService.cs
@@ -1,4 +1,5 @@
 ﻿using Blooms___Bakes_Boutique.Core.Models.Pastry;
+using Blooms___Bakes_Boutique.Enumerations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,5 +15,14 @@ namespace Blooms___Bakes_Boutique.Core.Contracts.Pastry
         Task<bool> PastryCategoryExistsAsync(int categoryId);
 
         Task<int> CreateAsync(PastryFormModel model, int patissierId);
+
+        Task<PastryQueryServiceModel> AllPastryAsync(
+            string? pastryCategory = null,
+            string? searchTerm = null,
+            PastrySorting sorting = PastrySorting.Newest,
+            int currentPage = 1,
+            int pastryPerPage = 1);
+
+        Task<IEnumerable<string>> AllPastryCategoriesNamesAsync();
 	}
 }
