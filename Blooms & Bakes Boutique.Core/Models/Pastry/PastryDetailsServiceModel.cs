a8dfd0d2dfcd1734d967bba5b0980393dd1b31d9
DimitarMalamski <mitemalamata@gmail.com>
2024-04-14 23:32:06 +0300

    PastryDetailsServiceModel class added

---
+++ Blooms & Bakes Boutique.Core/Models/Pastry/PastryDetailsServiceModel.cs
@@ -0,0 +1,18 @@
+﻿using Blooms___Bakes_Boutique.Core.Models.Patissier;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blooms___Bakes_Boutique.Core.Models.Pastry
+{
+	public class PastryDetailsServiceModel : PastryServiceModel
+	{
+		public string Recipe { get; set; } = null!;
+
+		public string PastryCategory { get; set; } = null!;
+
+		public PatissierServiceModel Patissier { get; set; } = null!;
+    }
+}
