74a8d5ac2fbcd420fd58cd32a64a71bd0b32cbb4
DimitarMalamski <mitemalamata@gmail.com>
2024-04-14 23:31:58 +0300

    FlowerDetailsServiceModel class added

---
+++ Blooms & Bakes Boutique.Core/Models/Flower/FlowerDetailsServiceModel.cs
@@ -0,0 +1,19 @@
+﻿using Blooms___Bakes_Boutique.Core.Models.Florist;
+using Blooms___Bakes_Boutique.Core.Models.Patissier;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blooms___Bakes_Boutique.Core.Models.Flower
+{
+	public class FlowerDetailsServiceModel : FlowerServiceModel
+	{
+		public string Description { get; set; } = null!;
+
+		public string FlowerCategory { get; set; } = null!;
+
+		public FloristServiceModel Florist { get; set; } = null!;
+	}
+}
