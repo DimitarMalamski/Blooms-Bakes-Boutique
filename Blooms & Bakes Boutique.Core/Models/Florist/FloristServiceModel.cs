2881daf1c98c342eba7f86cfd610ce7a1892ef07
DimitarMalamski <mitemalamata@gmail.com>
2024-04-14 23:30:56 +0300

    FloristServiceModel class added

---
+++ Blooms & Bakes Boutique.Core/Models/Florist/FloristServiceModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blooms___Bakes_Boutique.Core.Models.Florist
+{
+	public class FloristServiceModel
+	{
+		[Display(Name = "FlowerMaster Title")]
+		public string FlowerMasterTitle { get; set; } = null!;
+
+		public string UserName { get; set; } = null!;
+	}
+}
