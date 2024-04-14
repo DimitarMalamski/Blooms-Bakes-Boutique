38571563b119f66396953ca3d283648a49a16efd
DimitarMalamski <mitemalamata@gmail.com>
2024-04-14 18:26:44 +0300

    Extemsions folder added with classes for easing the mapping proccess

---
+++ Blooms & Bakes Boutique.Core/Extensions/IQuerableFlowerExtension.cs
@@ -0,0 +1,27 @@
+﻿using Blooms___Bakes_Boutique.Core.Models.Flower;
+using Blooms___Bakes_Boutique.Core.Models.Pastry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Linq
+{
+	public static class IQuerableFlowerExtension
+	{
+		public static IQueryable<FlowerServiceModel> ProjectToFlowerServiceModel(this IQueryable<Blooms___Bakes_Boutique.Infrastructure.Data.Models.Flowers.Flower> flowers)
+		{
+			return flowers
+				.Select(f => new FlowerServiceModel()
+				{
+					Id = f.Id,
+					Colour = f.Colour,
+					ImageUrl = f.ImageUrl,
+					IsGathered = f.GathererId != null,
+					PricePerBouquet = f.PricePerBouquet,
+					Title = f.Title
+				});
+		}
+	}
+}
---
+++ Blooms & Bakes Boutique.Core/Extensions/IQuerablePastryExtension.cs
@@ -0,0 +1,26 @@
+﻿using Blooms___Bakes_Boutique.Core.Models.Pastry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Linq
+{
+	public static class IQuerablePastryExtension
+	{
+		public static IQueryable<PastryServiceModel> ProjectToPastryServiceModel(this IQueryable<Blooms___Bakes_Boutique.Infrastructure.Data.Models.Pastries.Pastry> pastries)
+		{
+			return pastries
+				.Select(p => new PastryServiceModel()
+				{
+					Id = p.Id,
+					Description = p.Description,
+					ImageUrl = p.ImageUrl,
+					IsTasted = p.TasterId != null,
+					Price = p.Price,
+					Title = p.Title
+				});
+		}
+	}
+}
