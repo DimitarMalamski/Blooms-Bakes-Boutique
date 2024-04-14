bca1342b8094253eeaccb7187010199a6dffba21
DimitarMalamski <mitemalamata@gmail.com>
2024-04-14 18:26:59 +0300

    AllFlowerQueryModel finished

--- Blooms & Bakes Boutique.Core/Models/Flower/AllFlowersQueryModel.cs
+++ Blooms & Bakes Boutique.Core/Models/Flower/AllFlowersQueryModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Blooms___Bakes_Boutique.Core.Models.Pastry;
+using Blooms___Bakes_Boutique.Enumerations;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +11,22 @@ namespace Blooms___Bakes_Boutique.Core.Models.Flower
 {
 	public class AllFlowersQueryModel
 	{
+		public int FlowersPerPage { get; } = 3;
 
+		public string FlowerCategory { get; init; } = null!;
+
+		[Display(Name = "Search by text")]
+		public string SearchTerm { get; init; } = null!;
+
+		public FlowerSorting Sorting { get; init; }
+
+		public int CurrentPage { get; init; } = 1;
+
+		public int TotalFlowersCount { get; set; }
+
+		public IEnumerable<string> FlowerCategories { get; set; } = null!;
+
+		public IEnumerable<FlowerServiceModel> Flowers { get; set; } =
+			new List<FlowerServiceModel>();
 	}
 }
