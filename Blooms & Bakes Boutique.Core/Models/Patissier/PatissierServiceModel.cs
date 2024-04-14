3a3bd57ad6eecb8afcbedc90da549a479eaedcc5
DimitarMalamski <mitemalamata@gmail.com>
2024-04-14 23:31:12 +0300

    PatissierServiceModel class added

---
+++ Blooms & Bakes Boutique.Core/Models/Patissier/PatissierServiceModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blooms___Bakes_Boutique.Core.Models.Patissier
+{
+	public class PatissierServiceModel
+	{
+		[Display(Name = "MasterChef Title")]
+		public string MasterChefTitle { get; set; } = null!;
+
+		public string UserName { get; set; } = null!;
+    }
+}
