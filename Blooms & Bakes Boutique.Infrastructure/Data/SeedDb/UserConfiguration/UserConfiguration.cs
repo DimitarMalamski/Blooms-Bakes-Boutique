b86340c6fd279d0c2fc4292a907c5ff82a0066ef
DimitarMalamski <mitemalamata@gmail.com>
2024-04-15 22:28:57 +0300

    ApplicationUser added in UserConfiguration

--- Blooms & Bakes Boutique.Infrastructure/Data/SeedDb/UserConfiguration/UserConfiguration.cs
+++ Blooms & Bakes Boutique.Infrastructure/Data/SeedDb/UserConfiguration/UserConfiguration.cs
@@ -1,16 +1,17 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Blooms___Bakes_Boutique.Infrastructure.Data.Models.User;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Blooms___Bakes_Boutique.Infrastructure.Data.SeedDb.UserConfiguration
 {
-    internal class UserConfiguration : IEntityTypeConfiguration<IdentityUser>
+    internal class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
     {
-        public void Configure(EntityTypeBuilder<IdentityUser> builder)
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
         {
             var data = new SeedData();
 
-            builder.HasData(new IdentityUser[] { data.PatissierUser, data.FloristUser, data.GuestUser});
+            builder.HasData(new ApplicationUser[] { data.PatissierUser, data.FloristUser, data.GuestUser});
         }
     }
 }
