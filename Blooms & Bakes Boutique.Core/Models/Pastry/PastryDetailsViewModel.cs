0a267cb465eae343799c04d1193f2f13fa93b94b
DimitarMalamski <mitemalamata@gmail.com>
2024-04-15 03:37:46 +0300

    PastryDetailsViewModel class finished that will show before a deletion of a pastry

--- Blooms & Bakes Boutique.Core/Models/Pastry/PastryDetailsViewModel.cs
+++ Blooms & Bakes Boutique.Core/Models/Pastry/PastryDetailsViewModel.cs
@@ -8,6 +8,12 @@ namespace Blooms___Bakes_Boutique.Core.Models.Pastry
 {
 	public class PastryDetailsViewModel
 	{
+        public int Id { get; set; }
 
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string ImageUrl { get; set; } = string.Empty;
 	}
 }
