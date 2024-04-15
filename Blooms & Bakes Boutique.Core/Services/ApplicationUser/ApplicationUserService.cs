e730112094fda27f5b6c20261a8597b296302003
DimitarMalamski <mitemalamata@gmail.com>
2024-04-15 23:15:58 +0300

    FullName method implemented for each user

---
+++ Blooms & Bakes Boutique.Core/Services/ApplicationUser/ApplicationUserService.cs
@@ -0,0 +1,33 @@
+﻿using Blooms___Bakes_Boutique.Core.Contracts.ApplicationUser;
+using Blooms___Bakes_Boutique.Infrastructure.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blooms___Bakes_Boutique.Core.Services.ApplicationUser
+{
+    public class ApplicationUserService : IApplicationUserService
+    {
+        private readonly IRepository repository;
+        public ApplicationUserService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+        public async Task<string> UserFullNameAsync(string userId)
+        {
+            string result = string.Empty;
+
+            var user = await repository
+                .GetByIdAsync<Blooms___Bakes_Boutique.Infrastructure.Data.Models.User.ApplicationUser>(userId);
+
+            if (user != null)
+            {
+                result = $"{user.FirstName} {user.LastName}";
+            }
+
+            return result;
+        }
+    }
+}
