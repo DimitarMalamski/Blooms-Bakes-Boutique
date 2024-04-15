0c3b40bb0e293d502dfb9ce57a8d553f9a031ff6
DimitarMalamski <mitemalamata@gmail.com>
2024-04-15 03:39:42 +0300

    Exception class added for easing the work

---
+++ Blooms & Bakes Boutique.Core/Exceptions/UnauthorizedActionException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blooms___Bakes_Boutique.Core.Exceptions
+{
+	public class UnauthorizedActionException : Exception
+	{
+		public UnauthorizedActionException() { }
+
+		public UnauthorizedActionException(string message)
+			: base(message) { }
+	}
+}
